57ca82790e1300c7e81771916baeabac6986efec
Webprojon <tokhirjonkhasanov@gmail.com>
2025-03-18 21:13:11 +0100

    some pages are changed

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -1,11 +1,10 @@
 import { prisma } from "@/lib/db";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-	apiVersion: "2025-02-24.acacia",
-});
-
 export async function POST(request: Request) {
+	const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+		apiVersion: "2025-02-24.acacia",
+	});
 	const body = await request.text();
 
 	// verify that webhook is from Stripe
